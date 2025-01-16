37623028118bae47dc0a8d753aff71a6ee9af16b
gaku <akatigaku@gmail.com>
2025-01-16 23:50:33 +0900

    refactor parser, value

---
+++ src/value.rs
@@ -0,0 +1,21 @@
+#[derive(Debug, PartialEq, Eq, Clone)]
+pub enum Value<'src> {
+    Num(i32),
+    Op(&'src str),
+    Block(Vec<Value<'src>>),
+}
+
+impl <'src> Value<'src> {
+    pub fn as_block_vec(self) -> Vec<Value<'src>> {
+        match self {
+            Self::Block(val) => val,
+            _ => panic!("Value is not a block")
+        }
+    }
+    pub fn as_num(&self) -> i32 {
+        match self {
+            Self::Num(val) => *val,
+            _ => panic!("value is not number"),
+        }
+    }
+}
